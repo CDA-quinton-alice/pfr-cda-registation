3cc1f898c1853208edd48ccac25e3d35a73ae5ab
Mathieu AMBROISE <mat.ambroise@gmail.com>
2020-08-28 15:05:31 +0200

    Ajout UtilisateurServiceImpl

---
+++ projet-registation/src/main/java/fr/afpa/projetregistation/service/impl/UtilisateurServiceImpl.java
@@ -0,0 +1,7 @@
+package fr.afpa.projetregistation.service.impl;
+
+import fr.afpa.projetregistation.service.IUtilisateurService;
+
+public class UtilisateurServiceImpl implements IUtilisateurService {
+
+}
