10529f14f2ab939719fb5907976975416fe8c234
Mathieu AMBROISE <mat.ambroise@gmail.com>
2020-08-28 17:27:58 +0200

    Push avant de récup la dev

--- projet-registation/src/main/java/fr/afpa/projetregistation/entity/AdresseEntity.java
+++ projet-registation/src/main/java/fr/afpa/projetregistation/entity/AdresseEntity.java
@@ -5,12 +5,33 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Cette classe permet de définir les attributs de l'adresse
+ * 
+ * @author Mathieu
+ *
+ */
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class AdresseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-	
-	
+	private int idAdresse;
+	private int numero;
+	private String rue;
+	private String complement;
+	private int codePostal;
+	private String ville;
+	private String pays;
+
 }
