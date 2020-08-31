28be615a376aec2722cd6405c85846155405cea2
Alice Quinton <quinton.alice@gmail.com>
2020-08-31 10:18:16 +0200

    modifications dans IDocumentDao, ITypeMaterielDao & DocumentEntity
    modifications dans IDocumentDao, ITypeMaterielDao & DocumentEntity pour que tout fonctionne !!! youpiiii

--- projet-registation/src/main/java/fr/afpa/projetregistation/dao/ITypeMaterielDao.java
+++ projet-registation/src/main/java/fr/afpa/projetregistation/dao/ITypeMaterielDao.java
@@ -3,9 +3,11 @@ package fr.afpa.projetregistation.dao;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import fr.afpa.projetregistation.entity.TypeMaterielEntity;
 
+@Repository
 public interface ITypeMaterielDao extends CrudRepository<TypeMaterielEntity, Integer> {
 
 	Optional<TypeMaterielEntity> findByLibelleMateriel(String upperCase);
