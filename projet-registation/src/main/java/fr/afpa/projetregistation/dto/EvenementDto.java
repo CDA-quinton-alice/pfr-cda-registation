f7fa6362352c1ac4afcc656616ffbc24840371e9
Umek Samuel <samuel.umek@gmail.com>
2020-09-01 09:48:41 +0200

    JavaDoc EvenementDto

--- projet-registation/src/main/java/fr/afpa/projetregistation/dto/EvenementDto.java
+++ projet-registation/src/main/java/fr/afpa/projetregistation/dto/EvenementDto.java
@@ -20,11 +20,46 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class EvenementDto {
 	
+	/**
+	 * Identifiant unique de  l'évènement
+	 * de type @see Integer
+	 */
 	private int id;
+	
+	/**
+	 * Type de l'évènement parmis 4 choix :
+	 * 	- Autre
+	 *  - Panne
+	 *  - Révision
+	 *  - Inspection
+	 *  de type @see String
+	 */
 	private String type;
+	
+	/**
+	 * Description détailé de l'évènement
+	 * de type @see String
+	 */
 	private String description;
+	
+	/**
+	 * Date de début de l'évènement
+	 * de type @see Date
+	 */
 	private Date date_debut;
+	
+	/**
+	 * Date de fin de l'évènement
+	 * de type @see Date
+	 */
 	private Date date_fin;
+	
+	/**
+	 * Durée de l'évènement exprimé en
+	 * seconde(s) de type @see Integer
+	 */
 	private int duree;
+	
+	
 //	private int idUtilisateur;
 }
