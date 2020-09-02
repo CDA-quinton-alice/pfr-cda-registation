4c459c3c2851adc5d822fe41a37928a7ebc8869d
Mathieu AMBROISE <mat.ambroise@gmail.com>
2020-09-02 09:54:03 +0200

    Clean perso data insertion utilisateurs

--- projet-registation/src/test/java/fr/afpa/projetregistation/service/impl/test/TestDocumentServiceImpl.java
+++ projet-registation/src/test/java/fr/afpa/projetregistation/service/impl/test/TestDocumentServiceImpl.java
@@ -1,7 +1,8 @@
 package fr.afpa.projetregistation.service.impl.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.Date;
 
