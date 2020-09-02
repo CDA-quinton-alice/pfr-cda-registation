1e7555b46034839275a392da05e8e540c3e84377
Mathieu AMBROISE <mat.ambroise@gmail.com>
2020-09-02 12:14:10 +0200

    Echec lamentable sur tentative de test Dao, trop long, rollback
    Bien dans l'idée mais pas rentable

--- projet-registation/src/test/java/fr/afpa/projetregistation/dao/test/UtilisateurDaoTest.java
+++ projet-registation/src/test/java/fr/afpa/projetregistation/dao/test/UtilisateurDaoTest.java
@@ -16,8 +16,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import fr.afpa.projetregistation.dao.IAdresseDao;
+import fr.afpa.projetregistation.dao.IConnexionDao;
 import fr.afpa.projetregistation.dao.IUtilisateurDao;
-import fr.afpa.projetregistation.dto.UtilisateurDto;
+import fr.afpa.projetregistation.entity.AdresseEntity;
+import fr.afpa.projetregistation.entity.ConnexionEntity;
 import fr.afpa.projetregistation.entity.UtilisateurEntity;
 import fr.afpa.projetregistation.service.IUtilisateurService;
 
@@ -27,24 +30,87 @@ class UtilisateurDaoTest {
 
 	@Autowired
 	IUtilisateurService utilisateurService;
-	
+
 	@Autowired
 	IUtilisateurDao utilisateurDao;
-	
+
+	@Autowired
+	IAdresseDao adresseDao;
+
+	@Autowired
+	IConnexionDao connexionDao;
+
 	@Autowired
 	ModelMapper modelMapper;
+
+	AdresseEntity adresseTest = AdresseEntity.builder().numero(445).rue("rue du chat").complement("complément")
+			.codePostal("38000").ville("GRENOBLE").pays("FRANCE").build();
 	
+	ConnexionEntity connexionTest = ConnexionEntity.builder().matricule("EMPTEST001").password("pwd1").build();
+	
+	UtilisateurEntity employeEntity = UtilisateurEntity.builder().matricule("EMPTEST001").nom("nom1")
+			.prenom("prenom1").dateDeNaissance(dateNaissance()).salaire(2000.0).mail("mat@gmail.com").tel("06.06.06.06.06")
+			.responsable(false).connexion(connexionTest).adresse(adresseTest).build();
+	
+	/**
+	 * Initialisation d'un UtilisateurEntity pour les tests
+	 */
+	@Test
+	@Order(1)
+	void Ini() {
+		adresseDao.save(adresseTest);
+		connexionDao.save(connexionTest);
+		utilisateurDao.save(employeEntity);
+	}
+
 	@Autowired
 	/**
-	 * Permet de tester si l'utilisateur récuépéré par son nom (String)
-	 * correspond bien au bon Utilisateur
+	 * Permet de tester si l'utilisateur récuépéré par son nom (String) correspond
+	 * bien au bon Utilisateur
 	 * 
 	 * @return Optional UtilisateurEntity récupéré grâce au nom placé en paramètre
 	 */
 	@Test
-	@Order(1)
-	void findByNom() {
-		
+	@Order(2)
+	void findByNomTest() {
+		Optional<UtilisateurEntity> employeRecup = utilisateurDao.findByNom("nom1");
+		assertTrue(employeRecup.isPresent());
+		assertEquals("prenom1", employeRecup.get().getPrenom());
+	}
+
+	/**
+	 * Suppression de l'UtilisateurEntity, Adresse et Connexion de la bdd
+	 */
+	@Test
+	@Order(3)
+	void bye() {
+		utilisateurDao.delete(employeEntity);
+		adresseDao.delete(adresseTest);
+		connexionDao.delete(connexionTest);
+	}
+	/**
+	 * Permet de retourner une liste de tous les emlpoyés (boolean responsable =
+	 * false) Utilisation d'une requête native custom pour récupérer que les
+	 * employés Utilisation de la pagination
+	 * 
+	 * @param page
+	 * 
+	 * @return List de UtilisateurEntity qui sont des employés
+	 */
+//	@Query(nativeQuery = true, value = "SELECT * FROM utilisateur WHERE responsable = 0")
+//	Page<UtilisateurEntity> findAllEmployes(PageRequest page);
+
+	/**
+	 * Permet de retourner une liste de tous les responsables (boolean responsable =
+	 * true) Utilisation d'une requête native custom pour récupérer que les
+	 * responsables Utilisation de la pagination
+	 * 
+	 * @return List de UtilisateurEntity qui sont des responsables
+	 */
+//	@Query(nativeQuery = true, value = "SELECT * FROM utilisateur WHERE responsable = 1")
+//	Page<UtilisateurEntity> findAllResponsables(PageRequest page);
+
+	public Date dateNaissance() {
 		String pattern = "dd-MM-yyyy";
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
 		Date dateTest = null;
@@ -53,66 +119,7 @@ class UtilisateurDaoTest {
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-
-		//Construction UtilisateurEntity simples pour les tests
-		UtilisateurEntity employeEntity = UtilisateurEntity.builder()
-				.matricule("EMPTEST001")
-				.nom("nom1")
-				.prenom("prenom1")
-				.dateDeNaissance(dateTest)
-				.salaire(2000.0)
-				.mail("mat@gmail.com")
-				.tel("06.06.06.06.06")
-				.responsable(false)
-				.build();
-		
-		UtilisateurEntity responsableEntity = UtilisateurEntity.builder()
-				.matricule("RESPTEST001")
-				.nom("nom2")
-				.prenom("prenom2")
-				.dateDeNaissance(dateTest)
-				.salaire(2000.0)
-				.mail("mat@gmail.com")
-				.tel("06.06.06.06.06")
-				.responsable(false)
-				.build();
-		
-		utilisateurDao.save(employeEntity);
-		utilisateurDao.save(responsableEntity);
-		
-		Optional<UtilisateurEntity> employeRecup = utilisateurDao.findByNom("nom1");
-		assertTrue(employeRecup.isPresent());
-		assertEquals("prenom1" , employeRecup.get().getPrenom());
-		
-		Optional<UtilisateurEntity> responsableRecup = utilisateurDao.findByNom("nom2");
-		assertTrue(responsableRecup.isPresent());
-		assertEquals("prenom2" , responsableRecup.get().getPrenom());
-		
-		utilisateurDao.delete(employeEntity);
-		utilisateurDao.delete(responsableEntity);
+		return dateTest;
 	}
-
-	/**
-	 * Permet de retourner une liste de tous les emlpoyés 
-	 * (boolean responsable = false)
-	 * Utilisation d'une requête native custom pour récupérer que les employés
-	 * Utilisation de la pagination
-	 * @param page 
-	 * 
-	 * @return List de UtilisateurEntity qui sont des employés
-	 */
-//	@Query(nativeQuery = true, value = "SELECT * FROM utilisateur WHERE responsable = 0")
-//	Page<UtilisateurEntity> findAllEmployes(PageRequest page);
 	
-	/**
-	 * Permet de retourner une liste de tous les responsables 
-	 * (boolean responsable = true)
-	 * Utilisation d'une requête native custom pour récupérer que les responsables
-	 * Utilisation de la pagination
-	 * 
-	 * @return List de UtilisateurEntity qui sont des responsables
-	 */
-//	@Query(nativeQuery = true, value = "SELECT * FROM utilisateur WHERE responsable = 1")
-//	Page<UtilisateurEntity> findAllResponsables(PageRequest page);
-
 }
