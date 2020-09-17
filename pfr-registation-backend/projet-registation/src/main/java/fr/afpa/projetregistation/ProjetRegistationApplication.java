dbc794ed34e27d9fcca91629bf676113b961ec45
Mathieu AMBROISE <mat.ambroise@gmail.com>
2020-09-17 13:57:56 +0200

    Test en cours

--- pfr-registation-backend/projet-registation/src/main/java/fr/afpa/projetregistation/ProjetRegistationApplication.java
+++ pfr-registation-backend/projet-registation/src/main/java/fr/afpa/projetregistation/ProjetRegistationApplication.java
@@ -1,25 +1,28 @@
 package fr.afpa.projetregistation;
 
 import java.util.Collections;
-import java.util.stream.Stream;
 
 import org.apache.catalina.Context;
 import org.apache.tomcat.util.descriptor.web.JspConfigDescriptorImpl;
 import org.apache.tomcat.util.descriptor.web.JspPropertyGroup;
 import org.apache.tomcat.util.descriptor.web.JspPropertyGroupDescriptorImpl;
 import org.modelmapper.ModelMapper;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
-import fr.afpa.projetregistation.dao.IUserTestDao;
-import fr.afpa.projetregistation.entity.UserTestEntity;
+import fr.afpa.projetregistation.mail.EmailService;
+import fr.afpa.projetregistation.mail.Mail;
 
 @SpringBootApplication
 public class ProjetRegistationApplication {
+
+	@Autowired
+	private EmailService emailService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetRegistationApplication.class, args);
@@ -40,7 +43,7 @@ public class ProjetRegistationApplication {
 //            userRepository.findAll().forEach(System.out::println);
 //        };
 //    }
-	
+
 	@Bean
 	public ConfigurableServletWebServerFactory configurableServletWebServerFactory() {
 		return new TomcatServletWebServerFactory() {
@@ -60,4 +63,17 @@ public class ProjetRegistationApplication {
 			}
 		};
 	}
+
+	public void run(ApplicationArguments applicationArguments) throws Exception {
+	      System.out.println("coucou mail envoyé");
+
+	        Mail mail = new Mail();
+	        mail.setFrom("registationcdatest@gmail.com");
+	        mail.setTo("registationcdatest@gmail.com");
+	        mail.setSubject("Sending Simple Email with JavaMailSender Example");
+	        mail.setContent("This tutorial demonstrates how to send a simple email using Spring Framework.");
+
+	        emailService.sendSimpleMessage(mail);
+	    }
+	
 }
