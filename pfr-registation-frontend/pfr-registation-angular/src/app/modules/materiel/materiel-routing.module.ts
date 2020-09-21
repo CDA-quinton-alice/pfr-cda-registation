0961cf297b4e1180b2a2043fe752df72fb3e2759
aurelien vannier <aurelien.vannier@hotmail.fr>
2020-09-21 09:44:15 +0200

    ajout page localohost:4200/Materiel

--- pfr-registation-frontend/pfr-registation-angular/src/app/modules/materiel/materiel-routing.module.ts
+++ pfr-registation-frontend/pfr-registation-angular/src/app/modules/materiel/materiel-routing.module.ts
@@ -9,6 +9,7 @@ import { UpdateMaterielComponent } from './update-materiel/update-materiel.compo
 
 
 const routes: Routes = [
+  {path:'materiel', component:ListeMaterielComponent},
   {path:'materiel/liste', component:ListeMaterielComponent},
   {path:'materiel/infos', component:InfosMaterielComponent},
   {path:'materiel/ajouter', component:AjouterMaterielComponent},
