c406881a58076b12eea674637b79b0dff9b2b0c5
Alice Quinton <quinton.alice@gmail.com>
2020-09-22 12:47:40 +0200

    push de la liste des documents qui fonctionne
    --> tableau classique avec *ngFor

--- pfr-registation-frontend/pfr-registation-angular/src/app/modules/document/ajouter-document/ajouter-document.component.ts
+++ pfr-registation-frontend/pfr-registation-angular/src/app/modules/document/ajouter-document/ajouter-document.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {DocumentService} from '../../../services/document.service';
-import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRoute, Route, Router} from '@angular/router';
 import {IDocument} from '../../../interfaces/idocument';
 
-// Bonjour Mathieu Ambroise ! Aimes tu l'ambroisie ?
 
 @Component({
   selector: 'app-ajouter-document',
@@ -12,14 +11,29 @@ import {IDocument} from '../../../interfaces/idocument';
   styleUrls: ['./ajouter-document.component.css']
 })
 export class AjouterDocumentComponent {
-  // todayNumber: number = Date.now();
-  // todayDate: Date = new Date();
-  // todayString: string = new Date().toDateString();
-  // todayISOString: string = new Date().toISOString();
-  // todayNumber2: number = Dat.now();
-  // todayDate2: Date = new Date();
-  // todayString: string = new Date().toDateString();
-  // todayISOString2: string = new Date().toISOString();
+  document: IDocument;
+  documentList: Array<IDocument> = [];
+  date1: Date = new Date();
+  date2: Date = new Date();
+  todayNumber: number = Date.now();
+  todayDate: Date = new Date();
+  todayString: string = new Date().toDateString();
+  todayISOString: string = new Date().toISOString();
+  todayNumber2: number = Date.now();
+  todayDate2: Date = new Date();
+  todayString2: string = new Date().toDateString();
+  todayISOString2: string = new Date().toISOString();
+  documentForm = this.fb.group({
+    nomDocument: ['', [Validators.required]],
+    categorieDocument: ['', [Validators.required]],
+    dateAjoutDocument: [this.date1, [Validators.required]],
+    dateDerniereModificationDocument: [this.date2, [Validators.required]],
+    descriptionDocument: ['', [Validators.required]],
+    commentairesDocument: ['', [Validators.required]],
+    matriculeUtilisateur: ['', [Validators.required]],
+  })
+
+
   // documentForm = new FormGroup({
   // nomDocument: new FormControl('', Validators.required),
   //  categorieDocument = new FormControl('', Validators.required),
@@ -30,17 +44,20 @@ export class AjouterDocumentComponent {
   //   matriculeUtilisateur: new FormControl('', Validators.required),
   // });
 
-  document: IDocument;
-  listeDocuments: Array<IDocument> = [];
-
-  constructor(private route: ActivatedRoute, private router: Router, private documentService: DocumentService) {
+  constructor(private route: ActivatedRoute, private router: Router, private documentService: DocumentService, private fb: FormBuilder) {
   }
-
-  addDocument(){
+  addMateriel(){
+    this.document = (this.documentForm.value as IDocument);
     this.documentService.addDocument(this.document).subscribe(result => this.goToDocumentList());
   }
-
   goToDocumentList(){
     this.router.navigate(['document/liste']);
   }
+  // addDocument(){
+  //   this.documentService.addDocument(this.document).subscribe(result => this.goToDocumentList());
+  // }
+  //
+  // goToDocumentList(){
+  //   this.router.navigate(['document/liste']);
+  // }
 }
--- pfr-registation-frontend/pfr-registation-angular/src/app/modules/document/liste-document/liste-document.component.ts
+++ pfr-registation-frontend/pfr-registation-angular/src/app/modules/document/liste-document/liste-document.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {IDocument} from '../../../interfaces/idocument';
+import {DocumentService} from '../../../services/document.service';
 
 @Component({
   selector: 'app-liste-document',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListeDocumentComponent implements OnInit {
 
-  constructor() { }
+  document: IDocument;
+  documentList: Array<IDocument> = [];
+
+  constructor(private documentService: DocumentService) { }
 
   ngOnInit(): void {
+    this.documentService.findAll().subscribe(data => {
+      this.documentList = data;
+    });
   }
 
 }
