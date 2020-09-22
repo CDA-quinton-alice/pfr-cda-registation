0f93aaabcc6406bb0923e38df32b9b0397a8ab8a
aurelien vannier <aurelien.vannier@hotmail.fr>
2020-09-22 09:16:50 +0200

    Suppression user d'angular et déplacement du materiel service

--- pfr-registation-frontend/pfr-registation-angular/src/app/modules/materiel/ajouter-materiel/ajouter-materiel.component.ts
+++ pfr-registation-frontend/pfr-registation-angular/src/app/modules/materiel/ajouter-materiel/ajouter-materiel.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Imateriel } from 'src/app/interfaces/imateriel';
-import { MaterielService } from 'src/app/services/materiel.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MaterielService } from 'src/app/services/materiel-service/materiel.service';
 
 
 @Component({
