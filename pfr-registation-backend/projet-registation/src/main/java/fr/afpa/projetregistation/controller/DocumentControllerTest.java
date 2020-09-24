5efdc4a671e0b5e3a601a9f8d9d8a5280d3031db
Alice Quinton <quinton.alice@gmail.com>
2020-09-24 14:56:08 +0200

    maj interface angular document + controller documenttest
    allègement pour l'un et étoffement pour l'autre

--- pfr-registation-backend/projet-registation/src/main/java/fr/afpa/projetregistation/controller/DocumentControllerTest.java
+++ pfr-registation-backend/projet-registation/src/main/java/fr/afpa/projetregistation/controller/DocumentControllerTest.java
@@ -10,26 +10,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.afpa.projetregistation.dao.IDocumentTestDao;
+import fr.afpa.projetregistation.dto.DocumentDto;
 import fr.afpa.projetregistation.entity.DocumentTestEntity;
+import fr.afpa.projetregistation.service.IDocumentService;
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
+
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 
 public class DocumentControllerTest {
 
 	@Autowired
-	IDocumentTestDao documentDao;
+	IDocumentService documentService;
+
+
 
 	@GetMapping("/documenttest")
-    public List<DocumentTestEntity> getDocuments() {
-        return (List<DocumentTestEntity>) documentDao.findAll();
+    public List<DocumentDto> getDocuments() {
+		List<DocumentDto> documentsListe = documentService.getAllDocuments(1);
+        return documentsListe;
     }
 
     @PostMapping("/documenttest")
-    void addDocument(@RequestBody DocumentTestEntity document) {
-    	documentDao.save(document);
+    void addDocument(@RequestBody DocumentDto document) {
+    	documentService.ajouterDocument(document);
     }
 
 
