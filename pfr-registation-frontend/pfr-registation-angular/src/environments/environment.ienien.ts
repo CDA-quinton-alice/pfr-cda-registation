9b8cca87ba023b733f9c89dba696d151ffd75243
aurelien vannier <aurelien.vannier@hotmail.fr>
2020-10-09 09:23:28 +0200

    push début https

--- pfr-registation-frontend/pfr-registation-angular/src/environments/environment.ienien.ts
+++ pfr-registation-frontend/pfr-registation-angular/src/environments/environment.ienien.ts
@@ -1,7 +1,7 @@
 export const environment = {
     ienien: true,
-    backServer:'192.168.1.53:8080',
-    backSchema:'http',
+    backServer:'192.168.1.53:8443',
+    backSchema:'https',
     production: false,
     mathieu: false
   };
