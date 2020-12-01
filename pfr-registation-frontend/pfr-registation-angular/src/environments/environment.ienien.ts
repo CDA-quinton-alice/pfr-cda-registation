81669fcb7ac0b61e965dc65f09e686395320b487
aurelien vannier <aurelien.vannier@hotmail.fr>
2020-12-01 22:55:26 +0100

    maj config serveurs aurelien

--- pfr-registation-frontend/pfr-registation-angular/src/environments/environment.ienien.ts
+++ pfr-registation-frontend/pfr-registation-angular/src/environments/environment.ienien.ts
@@ -1,6 +1,6 @@
 export const environment = {
     ienien: true,
-    backServer:'192.168.1.53:8080',
+    backServer:'192.168.1.9:8080',
     backSchema:'http',
     production: false,
     mathieu: false
