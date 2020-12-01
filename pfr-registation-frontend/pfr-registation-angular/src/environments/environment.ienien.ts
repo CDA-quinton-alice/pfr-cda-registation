49d4fde09ea612474ae61b268ce7766d29303ab8
aurelien vannier <aurelien.vannier@hotmail.fr>
2020-12-01 22:43:11 +0100

    WIP: reprise pfr

--- pfr-registation-frontend/pfr-registation-angular/src/environments/environment.ienien.ts
+++ pfr-registation-frontend/pfr-registation-angular/src/environments/environment.ienien.ts
@@ -1,7 +1,7 @@
 export const environment = {
     ienien: true,
-    backServer:'192.168.1.53:8443',
-    backSchema:'https',
+    backServer:'192.168.1.53:8080',
+    backSchema:'http',
     production: false,
     mathieu: false
   };
